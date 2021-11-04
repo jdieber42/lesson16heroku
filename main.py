d9913371974786d745ffea55d889f44a39a6bf94
Jörg Dieber <joerg.dieber@gmail.com>
2021-11-04 19:25:36 +0100

    Added Jinja templates and variables.

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 webapp = Flask(__name__)
 
 @webapp.route("/")
 def index():
-    return render_template("index.html")
+    namen = "Hans"
+    return render_template("index.html", namen=namen, datum_today=datetime.now())
+
+@webapp.route("/about")
+def about():
+    authors = ["Hans", "Heike", "Monika"]
+
+    return render_template("about.html", authors=authors)
+
+@webapp.route("/projects")
+def projects():
+    projects = ["Flask Projects", "Boogle", "Fakebook"]
+
+    return render_template("projects.html", projects=projects)
 
 if __name__ == '__main__':
     webapp.run()
